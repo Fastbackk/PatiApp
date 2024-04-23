898cdb99520a72664a1e6fed1ddcbad1a6a55b48
mahmud <mahmuttunahan0029@gmail.com>
2024-04-23 15:20:36 +0300

    tuna

--- app/src/main/java/com/example/patiapp/MainActivity2.java
+++ app/src/main/java/com/example/patiapp/MainActivity2.java
@@ -2,13 +2,50 @@ package com.example.patiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.patiapp.databinding.ActivityMain2Binding;
 
 public class MainActivity2 extends AppCompatActivity {
+    private ActivityMain2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
+
+    public void secenek1(View view){
+
+        Intent intent=new Intent(MainActivity2.this, MainActivity.class);
+        intent.putExtra("ilan","ilanhayvan");
+        startActivity(intent);
+        finish();
+
+
+    }
+    public void secenek2(View view){
+
+        Intent intent=new Intent(MainActivity2.this, MainActivity.class);
+        intent.putExtra("ilan","ilanbakici");
+        startActivity(intent);
+        finish();
+
+
+    }
+    public void secenek3(View view){
+
+        Intent intent=new Intent(MainActivity2.this, MainActivity.class);
+        intent.putExtra("ilan","ilanmama");
+        startActivity(intent);
+        finish();
+
+
+    }
+
 }
