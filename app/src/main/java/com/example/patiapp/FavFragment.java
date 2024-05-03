e09eda0f69ddd960527b7ccaddff8091823be760
Kaytiue7 <vlkn.gms7@gmail.com>
2024-05-03 18:26:50 +0300

    ronaldo

--- app/src/main/java/com/example/patiapp/FavFragment.java
+++ app/src/main/java/com/example/patiapp/FavFragment.java
@@ -1,19 +1,114 @@
 package com.example.patiapp;
 
+import static android.content.Intent.getIntent;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.patiapp.databinding.FragmentFavBinding;
+import com.example.patiapp.databinding.FragmentIlanlarBinding;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 
 public class FavFragment extends Fragment {
+    private FragmentFavBinding binding;
+    ArrayList<Post>messageArrayList;
+    Adapter adapter;
+    private FirebaseFirestore firebaseFirestore;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        messageArrayList=new ArrayList<>();
+        firebaseFirestore= FirebaseFirestore.getInstance();
+        getData();
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fav, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentFavBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.recyclerView6.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter=new Adapter(messageArrayList);
+        binding.recyclerView6.setAdapter(adapter);
+
+    }
+    public void getData(){
+        firebaseFirestore.collection("Ilanlar").orderBy("date", Query.Direction.DESCENDING)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if(error!=null){
+                            Toast.makeText(getContext(), error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                            return; // Hata olduğunda işlemi durdur.
+                        }
+                        if(value!=null){
+                            messageArrayList.clear(); // Listenin her veri çekilişinde temizlenmesi önemli.
+                            for (DocumentSnapshot snapshot : value.getDocuments()){
+                                Map<String, Object> data = snapshot.getData();
+
+                                assert data != null;
+                                String baslik = (String) data.get("ilanbaslik");
+                                String dowloandurl = (String) data.get("dowloandurl");
+                                String sehir = (String) data.get("sehir");
+                                String ilanturu = (String) data.get("ilanturu");
+                                String date = null;
+
+                                Object dateObj = data.get("date");
+                                if (dateObj instanceof Timestamp) {
+                                    Timestamp timestamp = (Timestamp) dateObj;
+                                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+                                    date = sdf.format(timestamp.toDate());
+                                } else if (dateObj instanceof String) {
+                                    // String olarak kaydedilmişse, bu blok çalışacak
+                                    date = (String) dateObj;
+                                }
+
+                                Post ilan = new Post(baslik, dowloandurl, sehir, ilanturu, date);
+                                messageArrayList.add(ilan);
+                            }
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
 }
