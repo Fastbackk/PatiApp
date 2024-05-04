4961944761b9a6be0441dad54f588719b749e6a8
mahmud <mahmuttunahan0029@gmail.com>
2024-05-04 18:54:28 +0300

    yorum

---
+++ app/src/main/java/com/example/patiapp/HesapKayitSorgu.java
@@ -0,0 +1,27 @@
+package com.example.patiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class HesapKayitSorgu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hesap_kayit_sorgu);
+    }
+    public void barinak(View view){
+        Intent intent=new Intent(HesapKayitSorgu.this,BarinakHesapOlustur.class);
+        startActivity(intent);
+        finish();
+    }
+    public void back(View view){
+        Intent intent=new Intent(HesapKayitSorgu.this,HesapKayit.class);
+        startActivity(intent);
+        finish();
+
+    }
+}
